84aefd846b44b90e6a420512d11a67a5ee57579f
Rafael Balaguera <rafaelbalaguera@gmail.com>
2026-08-13 12:38:08 -0500

    Revertir AppBar al texto 'RocaKids' (el logo no se veía bien ahí)

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -14,8 +14,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: false,
-        title: Image.asset('assets/images/logo_rocakids_compacto.png', height: 40, fit: BoxFit.contain),
+        title: const Text('RocaKids'),
         actions: [
           IconButton(
             icon: const Icon(Icons.person),
